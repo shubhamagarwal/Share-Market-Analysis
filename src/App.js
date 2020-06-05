9506e7fe7ccf41e4ab096e6a8ea111b5a381ddc7
shubham Agarwal <shubham150@gmail.com>
2020-06-05 19:54:16 +0530

    Added code for dropdown and highcharts

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import React from 'react';
  import Header from './views/Header';
  import Chart from './views/Chart';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state  = {
-    };
-  }
-  
-   render() {
-     return (
-       <div className="app-container">
-         <Header />
-         <Chart dataLine={this.state.dataLine} />
-       </div>
-     );
-   }
- }
+const App = () => {
+  return (
+    <div className="app-container">
+      <Header />
+      <Chart />
+    </div>
+  );
+}
  
- export default App;
+ 
+ export default React.memo(App);
--- src/views/Header.js
+++ src/views/Header.js
@@ -1,40 +1,53 @@
-import React from 'react';
-import './Header.css';
-import logo from '../images/Stashaway-logo.png'
+import React from "react";
+import "./Header.css";
+import logo from "../images/Stashaway-logo.png";
 // import logo from './../logo.png';
 
 const Header = () => {
-    return(
-        <>
-            <header>
-                
-                <nav>
-                    
-                    <div className="row">
-                        <img src={logo} alt="StashAway" className="stash-logo" />
-                        <ul class="main-nav">
-                            <li><a href="#features">Food delivery</a></li>
-                            <li><a href="#works">How it works</a></li>
-                            <li><a href="#cities">Our cities</a></li>
-                            <li><a href="#plans">Sign up</a></li>
-                        </ul>
-                        <div>
-                            <h4>Overview</h4>
-                            <div>
-                            <ul class="main-nav sec-nav">
-                                <li><a href="#features">Food delivery</a></li>
-                                <li><a href="#works">How it works</a></li>
-                                <li><a href="#cities">Our cities</a></li>
-                                <li><a href="#plans">Sign up</a></li>
-                            </ul>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </header>
-        
-        </>
-    )
-}
+  return (
+    <>
+      <header>
+        <nav>
+          <div className="row">
+            <img src={logo} alt="StashAway" className="stash-logo" />
+            <ul className="main-nav">
+              <li>
+                <a href="#features">Food delivery</a>
+              </li>
+              <li>
+                <a href="#works">How it works</a>
+              </li>
+              <li>
+                <a href="#cities">Our cities</a>
+              </li>
+              <li>
+                <a href="#plans">Sign up</a>
+              </li>
+            </ul>
+            <div>
+              <h4>Overview</h4>
+              <div>
+                <ul className="main-nav sec-nav">
+                  <li>
+                    <a href="#features">Food delivery</a>
+                  </li>
+                  <li>
+                    <a href="#works">How it works</a>
+                  </li>
+                  <li>
+                    <a href="#cities">Our cities</a>
+                  </li>
+                  <li>
+                    <a href="#plans">Sign up</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
 
 export default Header;
